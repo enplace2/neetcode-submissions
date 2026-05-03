cc65cc0da6cd4dd5eaf5d40c41396505dc18dee9
Nick Place <114956247+enplace2@users.noreply.github.com>
2026-05-03 13:41:23 -0500

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const counts = {}
+
+        for (const num of nums){
+            if(!counts[num]){
+                counts[num] = 1
+            }else{
+                counts[num]++
+            }
+        }
+
+        const groupedVals = new Array(nums.length +1)
+
+        for(const [num, count] of Object.entries(counts)){
+            if(!Array.isArray(groupedVals[count])){
+                groupedVals[count] = []
+            }
+            groupedVals[count].push(num)
+        }
+
+        // loop backwards through the array until we get the count we need
+        const result = []
+        for (let i = groupedVals.length -1; i >=0;i-- ){
+            const groupedNums = groupedVals[i];
+            if(!Array.isArray(groupedVals[i])){
+                continue;
+            }
+            for (const num of groupedNums){
+                result.push(num)
+            }
+            if(result.length === k){
+                return result
+            }
+        }
+
+        return result;
+    }
+}
